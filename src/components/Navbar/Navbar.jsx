621fdab5ad511208469bc6057959fd971d877551
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-12-02 22:56:45 -0600

    Add nav bar tanimation and burger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+// Navbar.jsx
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import styles from './navbar.module.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
-      <Link activeClass={styles.active} to="portfolio" spy={true} smooth={true} offset={-70} duration={500}>
-        Portfolio
-      </Link>
-      <Link activeClass={styles.active} to="apps" spy={true} smooth={true} offset={-70} duration={500}>
-        Apps
-      </Link>
-      <Link activeClass={styles.active} to="about" spy={true} smooth={true} offset={-70} duration={500}>
-        About Me
-      </Link>
-      <Link activeClass={styles.active} to="contact" spy={true} smooth={true} offset={-70} duration={500}>
-        Contact
-      </Link>
-      <Link activeClass={styles.active} to="socials" spy={true} smooth={true} offset={-70} duration={500}>
-        Socials
-      </Link>
+      <div className={styles.menuIcon} onClick={() => setIsOpen(!isOpen)}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      <div className={`${styles.navItems} ${isOpen ? styles.showNav : ''}`}>
+        <Link activeClass={styles.active} to="portfolio" spy={true} smooth={true} offset={-70} duration={500}>
+          Portfolio
+        </Link>
+        <Link activeClass={styles.active} to="apps" spy={true} smooth={true} offset={-70} duration={500}>
+          Apps
+        </Link>
+        <Link activeClass={styles.active} to="about" spy={true} smooth={true} offset={-70} duration={500}>
+          About Me
+        </Link>
+        <Link activeClass={styles.active} to="contact" spy={true} smooth={true} offset={-70} duration={500}>
+          Contact
+        </Link>
+        <Link activeClass={styles.active} to="socials" spy={true} smooth={true} offset={-70} duration={500}>
+          Socials
+        </Link>
+      </div>
     </nav>
   );
 }
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,8 +5,8 @@ import styles from './portfolio.module.css';
 function Portfolio() {
   return (
     <section id="portfolio" className={styles.portfolio}>
-      <h1>My Portfolio</h1>
-      <p>Here you will find my work and projects that I've been involved with.</p>
+      <h1>Rudi Carrillo</h1>
+      <p>As a software developer, my passion is crafting seamless solutions, embracing both front and back-end challenges with a unique blend of creativity, technical expertise, and entrepreneurial spirit. My approach mirrors the dynamism of combining an aesthetic eye, problem-solving skills, and strategic drive. This fusion enables me to deliver innovative and efficient software solutions, making me a versatile and valuable asset to any team aiming to transform ideas into reality..</p>
       {/* Insert portfolio items here */}
     </section>
   );
