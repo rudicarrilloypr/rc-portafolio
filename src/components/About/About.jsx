38156b01687fc8fc7f9e341e6387fa379c373ef2
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-12-12 02:11:38 -0600

    Add my stack section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,63 @@
 import React from 'react';
-import { FaAward, FaReact, FaJsSquare } from 'react-icons/fa';
+import { FaAward, FaReact, FaJsSquare, FaHtml5, FaCss3, FaTerminal, FaStackOverflow  } from 'react-icons/fa';
 import { PiFileCssDuotone } from "react-icons/pi";
 import { PiFileHtmlDuotone } from "react-icons/pi";
-import { DiRubyRough } from "react-icons/di";
-import { SiRubyonrails } from "react-icons/si";
+import { DiRubyRough, DiRuby  } from "react-icons/di";
+import { SiRubyonrails, SiRubygems, SiSelenium } from "react-icons/si";
 import { BiLogoRedux } from "react-icons/bi";
+import { GiCapybara } from "react-icons/gi";
 import { FaRegFileCode } from "react-icons/fa6";
+import { TbKeyframes } from "react-icons/tb";
+import { IoIosCheckmarkCircle } from "react-icons/io";
 import profileImage from '../../assets/rudicarrillo-circle.PNG';
 import styles from './about.module.css';
 
 function About() {
   return (
     <section id="about" className={styles.about}>
+      <h1>My Stack</h1>
+    {/* Sección de habilidades */}
+    <div className={styles.skillsSection}>
+      <div className={styles.skillBox}>
+        <FaStackOverflow className={styles.skillIcon} />
+        <h3>Languages</h3>
+        <ul>
+          <li><FaJsSquare /> JavaScript</li>
+          <li><DiRuby /> Ruby</li>
+          <li><FaHtml5 /> HTML</li>
+          <li><FaCss3 /> CSS</li>
+        </ul>
+      </div>
+      <div className={styles.skillBox}>
+        <TbKeyframes className={styles.skillIcon} />
+        <h3>Frameworks</h3>
+        <ul>
+          <li><FaReact /> React.js</li>
+          <li><SiRubyonrails /> Ruby on Rails</li>
+          <li><SiRubygems /> RSpec</li>
+          <li><GiCapybara /> Capybara</li>
+          <li><SiSelenium /> Selenium</li>
+        </ul>
+      </div>
+      <div className={styles.skillBox}>
+        <FaTerminal className={styles.skillIcon} />
+        <h3>Skills</h3>
+        <ul>
+          <li><IoIosCheckmarkCircle /> Database Management</li>
+          <li><IoIosCheckmarkCircle /> Version Control</li>
+          <li><IoIosCheckmarkCircle /> CLI</li>
+          <li><IoIosCheckmarkCircle /> Web Development</li>
+          <li><IoIosCheckmarkCircle /> API Design</li>
+        </ul>
+      </div>
+    </div>
+
       <h1>About Me</h1>
       <div className={styles.quoteSection}>
       <div className={styles.profileImageWrapper}>
   <img src={profileImage} alt="Rudi Carrillo" className={styles.profileImage}/>
 </div>
-<a href="https://drive.google.com/file/d/18ePzDb0wrpynwiEKyil2uMHdhFAddWCl/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className={styles.resumeButton}>My Resume</a>
+<a href="https://drive.google.com/file/d/18ePzDb0wrpynwiEKyil2uMHdhFAddWCl/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className={styles.resumeButton}>Get my Resume</a>
         <p className={styles.quote}>"As a software developer, my passion is crafting seamless solutions, embracing both front and back-end challenges with a unique blend of creativity, technical expertise, and entrepreneurial spirit. My approach mirrors the dynamism of combining an aesthetic eye, problem-solving skills, and strategic drive. This fusion enables me to deliver innovative and efficient software solutions, making me a versatile and valuable asset to any team aiming to transform ideas into reality..."</p>
         <p className={styles.author}>- Rudi Carrillo</p>
       </div>
